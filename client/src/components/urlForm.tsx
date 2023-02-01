fca7002a54b424621e99fd9a1aa301ce234ca81c
Kasun Abaywardana <abaywardanakasun@gmail.com>
2023-02-01 09:16:54 +0800

    FE validation for URL format

--- client/src/components/urlForm.tsx
+++ client/src/components/urlForm.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Alert, Button, Card, Form, InputGroup } from 'react-bootstrap';
 import { UrlType } from '../models/urlModel';
 import { createShortUrl } from '../services/urlService';
 
 const UrlForm = () => {
     const [inputUrl, setInputUrl] = useState('');
+    const [isValidUrl, setIsValidUrl] = useState({ status: false, message: '' });
     const [createdUrl, setCreatedUrl] = useState({} as UrlType);
+
+    useEffect(() => {
+        // checkValidityURL();
+    }, [])
+
 
     const saveUrl = () => {
         createShortUrl({ longUrl: inputUrl } as UrlType).then((res) => {
@@ -21,9 +27,10 @@ const UrlForm = () => {
         });
     }
 
-    const onUrlValueChange = (event: any) => {
+    const onUrlValueChange = async (event: any) => {
         const longUrl = event.target.value;
-        setInputUrl(longUrl);
+        await setInputUrl(longUrl);
+        checkValidityURL();
     }
 
     const resetUrl = () => {
@@ -31,23 +38,39 @@ const UrlForm = () => {
         setInputUrl('');
     }
 
+    const checkValidityURL = () => {
+        const urlPattern = new RegExp(
+            /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+        );
+        const urlValidity = urlPattern.test(inputUrl);
+        if (urlValidity) {
+            setIsValidUrl({ status: true, message: '' });
+        } else {
+            setIsValidUrl({ status: false, message: 'Incorrect URL format' });
+        }
+    }
+
     return (
         <Card className='mt-5 w-auto'>
             <Card.Header>Convert URL</Card.Header>
             <Card.Body className="text-start">
-                <Form.Label htmlFor="basic-url">Input your long URL and click convert to short</Form.Label>
-                <InputGroup className="mb-3">
-                    <Form.Control
-                        placeholder="Long URL"
-                        aria-label="Long URL"
-                        aria-describedby="basic-addon2"
-                        onChange={onUrlValueChange}
-                        value={inputUrl}
-                    />
-                    <Button variant="outline-primary" id="button-short" disabled={inputUrl === '' || inputUrl === undefined ? true : false} onClick={saveUrl}>
-                        Short
-                    </Button>
-                </InputGroup>
+                <Form noValidate >
+                    <Form.Label htmlFor="basic-url">Input your long URL and click convert to short</Form.Label>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            placeholder="Long URL"
+                            aria-label="Long URL"
+                            aria-describedby="basic-addon2"
+                            onChange={onUrlValueChange}
+                            value={inputUrl}
+                            isInvalid={isValidUrl.message !== ''}
+                        />
+                        <Button variant="outline-primary" id="button-short" disabled={inputUrl === '' || inputUrl === undefined || isValidUrl.message !== '' ? true : false} onClick={saveUrl}>
+                            Short
+                        </Button>
+                        <Form.Control.Feedback type="invalid">{isValidUrl.message}</Form.Control.Feedback>
+                    </InputGroup>
+                </Form>
                 <Button variant="outline-primary" id="button-clear" onClick={resetUrl}>
                     Clear
                 </Button>
